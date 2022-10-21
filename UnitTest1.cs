08ca0012419cd5b58360aa969784daab2a377aee
Duaa Ahmad <ahmadd60@uwosh.edu>
2022-10-20 22:10:41 -0500

    Added final tests

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,10 +1,16 @@
 using Lab2Solution;
+using Npgsql;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab4Test
 {
     public class Tests
     {
         IBusinessLogic ibl;
+        public ObservableCollection<Entry> entries;
+       
+
         [SetUp]
         public void Setup()
         {
@@ -20,6 +26,8 @@ namespace Lab4Test
             String date = "10-20-2022";
 
             Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.NoError);
+
+            ibl.DeleteEntry(1);
         }
 
         [Test]
@@ -97,6 +105,121 @@ namespace Lab4Test
             String date = "13-200-2022";
 
             Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.InvalidDate);
+        }
+
+
+        [Test]
+        public void EditEntryTest()
+        {
+            ibl.AddEntry("Most depressed major", "CS", 2, "10-20-2022");
+
+            String clue = "Most depressed major";
+            String answer = "CS";
+            int difficulty = 2;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.EditEntry(clue, answer, difficulty, date, 1), EntryEditError.NoError);
+
+            ibl.DeleteEntry(1);
+        }
+
+        [Test]
+        public void EditEntryInvalidClueTest()
+        {
+            String clue = "Most depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed majorMost depressed major";
+            String answer = "Comp sci";
+            int difficulty = 2;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.EditEntry(clue, answer, difficulty, date, 1), EntryEditError.InvalidFieldError);
+        }
+
+        [Test]
+        public void EditEntryNullClueTest()
+        {
+            String clue = null;
+            String answer = "CS";
+            int difficulty = 2;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.EditEntry(clue, answer, difficulty, date, 1), EntryEditError.InvalidFieldError);
+        }
+
+        [Test]
+        public void EditEntryInvalidAnswerTest()
+        {
+            String clue = "Most depressed major";
+            String answer = "CSCSCSCSCSCSCSCSCSCSCS";
+            int difficulty = 2;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.EditEntry(clue, answer, difficulty, date, 1), EntryEditError.InvalidFieldError);
+        }
+
+        [Test]
+        public void EditEntryNullAnswerTest()
+        {
+            String clue = "Most depressed major";
+            String answer = null;
+            int difficulty = 2;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.InvalidAnswerLength);
+        }
+
+        [Test]
+        public void EditEntryDifficultyExceedsMaxTest()
+        {
+            String clue = "Most depressed major";
+            String answer = "CS";
+            int difficulty = 3;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.InvalidDifficulty);
+        }
+
+        [Test]
+        public void EditEntryNegativeDifficultyTest()
+        {
+            String clue = "Most depressed major";
+            String answer = "CS";
+            int difficulty = -1;
+            String date = "10-20-2022";
+
+            Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.InvalidDifficulty);
+        }
+
+        [Test]
+        public void EditEntryInvalidDate()
+        {
+            String clue = "Most depressed major";
+            String answer = "CS";
+            int difficulty = 2;
+            String date = "13-200-2022";
+
+            Assert.AreEqual(ibl.AddEntry(clue, answer, difficulty, date), InvalidFieldError.InvalidDate);
+        }
+
+        [Test]
+        public void DeleteEntryTest()
+        {
+            ibl.AddEntry("Most depressed major", "CS", 2, "10-20-2022");
+            Assert.AreEqual(ibl.DeleteEntry(1), EntryDeletionError.NoError);
+        }
+
+        [Test]
+        public void DeleteEntryInvalidIdTest()
+        {
+            Assert.AreEqual(ibl.DeleteEntry(1), EntryDeletionError.EntryNotFound);
+        }
+
+        [Test]
+        public void GetEntriesTest()
+        {
+            ibl.AddEntry("Most depressed major", "CS", 2, "10-20-2022");
+            int count = ibl.GetEntries().Count();
+            Assert.AreEqual(count, 1);
+            ibl.DeleteEntry(1);
         }
     }
 }
